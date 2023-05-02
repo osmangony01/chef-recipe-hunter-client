2b6c0e46cb67653b6e4ee33dfd01a86e17f75932
osmangony01 <mdosmangoni376@gmail.com>
2023-05-02 13:59:56 +0600

    complete navbar and header section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import NavBar from './NavBar/NavBar';
+import './Header.css';
+import { FaSearch } from 'react-icons/fa';
 
 const Header = () => {
     return (
-        <div>
-            this is header
+        <div className='header'>
+            <NavBar></NavBar>
+            <div className='banner-content'>
+                <h2>What are your favorite <br />food?</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae ullam, nostrum labore assumenda pariatur delectus praesentium quo quidem itaque dolorem harum modi? Animi laboriosam debitis dolorem impedit accusamus sequi quidem.</p>
+                <div className='search-filed '>
+                    <input className='search' type="text" placeholder='Search...'/>
+                    <span className='search-icon'><FaSearch color='white'></FaSearch></span>
+                </div>
+            </div>
         </div>
     );
 };
---
+++ src/components/Header/NavBar/NavBar.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.css';
+import { FaBars, FaTimes, FaUserCircle } from 'react-icons/fa';
+
+const NavBar = () => {
+    const [toggle, setToggle] = useState(true);
+
+    const handleToggle = () => {
+        setToggle(!toggle);
+    }
+    return (
+        <nav className="nav">
+            <div className='nav-title'>
+                <div className='toggle-icon' onClick={handleToggle}>
+                    {
+                        toggle ? <span className='hide'><FaBars size={24}></FaBars></span>
+                            : <span className='hide'><FaTimes size={25}></FaTimes></span>
+                    }
+                </div>
+                <div className='title-nav'>
+                    <img src="/images/nav-title-logo.jpg" alt="" width="60px" height="45px"
+                    />
+                    <h2>Flavor Frenzy</h2>
+                </div>
+                <div className='toggle-icon'>
+                    <img src="" alt="asdf" />
+                    <span className='hide'><FaUserCircle size={24}></FaUserCircle></span>
+                </div>
+            </div>
+            <div className={toggle ? "menu-list" : "menu-list menu-list-none"}>
+                <ul>
+                    <li><Link className="nav-link" to="/">Home</Link></li>
+                    <li><Link className="nav-link" to="/destination">Blog</Link></li>
+                    <li><Link className="nav-link" to="/blog">About</Link></li>
+                    <li><Link className="nav-link" to="/contact">Contact</Link></li>
+                    <li><Link className="nav-link" to="/login">Login</Link></li>
+                    <li className='user-icon'>
+                        <img src="" alt="asdf" />
+                        <span className=''><FaUserCircle size={24}></FaUserCircle></span>
+                    </li>
+
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
